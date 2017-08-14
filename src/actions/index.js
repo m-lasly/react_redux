ddc9a01df9aed227ef58f4da1dc6f6d1dc51baad
Maati Lasly <maati.lasly@adviqo.com>
2017-08-14 16:15:51 +0200

    add actions

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+export function selectBook(book) {
+
+  //selectBook is a ActionCreator it needs to return an action,
+  // an object with a type property,
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book
+  };
+}
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function (){
   return[
-    {title:'javascript:the Good Part'},
-    {title:'Harry Potter'},
-    {title:'The Dark Wall'},
-    {title:'Mule:the Good Part'}
+    {title:'javascript:the Good Part',pages:101},
+    {title:'Harry Potter',pages:122},
+    {title:'The Dark Wall',pages:201},
+    {title:'Mule:the Good Part',pages:11}
   ]
 }
